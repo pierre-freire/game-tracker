aef72c2431d4fed801a6b36695457f500f73101d
Pierre Freire <pierrefreire.profis@gmail.com>
2023-04-16 19:13:26 -0300

    Ordenação adicionada componente de ordenação adicionado

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import TheHeader from "./components/TheHeader.vue";
 import GameList from "./components/GameList.vue";
 import SearchBar from "./components/SearchBar.vue";
+import Sort from "./components/Sort.vue"
 </script>
 
 <template>
   <TheHeader />
   <SearchBar />
+  <Sort />
   <GameList />
 </template>
 
---
+++ src/components/Sort.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useGamesStore } from "../store/games";
+
+const gamesStore = useGamesStore();
+
+function searchValue(sortValue) {
+  if (sortValue.target.value.slice(-1) == "B") {
+    gamesStore.sortGames(sortValue.target.value.slice(0, -1), "bigger");
+    return;
+  }
+  gamesStore.sortGames(sortValue.target.value);
+}
+</script>
+
+<template>
+  <label class="input-wrapper">
+    <select class="input" v-model="searchText" @change="searchValue">
+      <option value="" disabled selected>Select an option</option>
+      <option value="savings">% de Desconto</option>
+      <option value="salePriceB">Maior Preço</option>
+      <option value="salePrice">Menor Preço</option>
+      <option value="title">Titulo</option>
+    </select>
+  </label>
+</template>
+
+<style scoped>
+</style> 
